c4caccd7cd81722f51b0b369f901fb3a9094d929
raxmatillo-ruziyev <raxmatillo248@gmail.com>
2024-06-25 20:54:47 +0500

    sss

---
+++ src/Login/Login.jsx
@@ -0,0 +1,80 @@
+import { Button, Input, Form, message } from 'antd';
+import axios from 'axios';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Login = () => {
+    const navigate = useNavigate();
+    
+    useEffect(() => {
+        if (!localStorage.getItem('access_token')) {
+            navigate('/');
+        }
+    }, [navigate]);
+
+    const onFinish = (values) => {
+        axios({
+            url: 'https://autoapi.dezinfeksiyatashkent.uz/api/auth/signin',
+            method: 'POST',
+            data: values,
+        })
+        .then(res => {
+            if (res.data.success) {
+                localStorage.setItem('access_token', res.data.data.tokens.accessToken.token);
+                message.success('Muvafaqiyatli kirdingiz');
+                navigate('/home');
+            } else {
+                message.error('Xatolik bor');
+            }
+        })
+        .catch(err => {
+            message.error('Xatolik bor: ' + err.message);
+        });
+    };
+
+    return (
+        <>
+            <Form
+                name="basic"
+                onFinish={onFinish}
+                autoComplete="off"
+                style={{ width: '500px', margin: '100px auto' }}
+                layout="vertical"
+            >
+                <Form.Item
+                    label="Phone"
+                    name="phone_number"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your phone number!',
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="Password"
+                    name="password"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your password!',
+                        },
+                    ]}
+                >
+                    <Input.Password />
+                </Form.Item>
+
+                <Form.Item>
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
+        </>
+    );
+};
+
+export default Login;
